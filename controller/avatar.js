d5f88b8d9a359fdf321e3898d8a4e369dfcc17e7
ka2nq1 <vlasiuk.aleks16@gmail.com>
2021-04-06 13:34:07 +0300

    avatar router

---
+++ controller/avatar.js
@@ -0,0 +1,41 @@
+const fs = require('fs').promises
+const path = require('path')
+const jimp = require('jimp')
+const User = require('../model/schema/user')
+
+const storeImage = path.join(process.cwd(), 'public', 'avatars')
+
+const createAvatar = async (id, file) => {
+  const name = `${id}.jpg`
+  const image = await jimp.read(file)
+  await image
+    .autocrop()
+    .cover(250, 250, jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_TOP)
+    .writeAsync(file)
+  await fs.rename(file, path.join(storeImage, name))
+
+  const avatar = `http://localhost:3000/avatars/${name}`
+  return avatar
+}
+
+const updateAvatar = async (req, res, next) => {
+  const id = req.user.id
+  const file = req.file.path
+
+  try {
+    const url = await createAvatar(id, file)
+    await User.updateOne({ _id: id }, { avatarURL: url })
+    return res.status(200).json({
+      status: 'success',
+      code: 200,
+      data: {
+        avatarURL: url,
+      }
+    })
+  } catch (err) {
+    await fs.unlink(file)
+    return next(err)
+  }
+}
+
+module.exports = { updateAvatar }
---
+++ routes/api/images.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const ctrlAvatar = require('../../controller/avatar')
+const guard = require('../../helpers/guard')
+const upload = require('../../helpers/fileUpload')
+
+router.patch('/avatars', guard, upload.single('avatar'), ctrlAvatar)
+
+module.exports = router
